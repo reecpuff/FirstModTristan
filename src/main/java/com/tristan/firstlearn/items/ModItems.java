77b226b59f74071da309666de951369ca16ecae1
Tristan <tristans.1234567890@gmail.com>
2025-06-01 01:48:36 -0400

    Fixed item registry and messed with resource directories

--- src/main/java/com/tristan/firstlearn/items/ModItems.java
+++ src/main/java/com/tristan/firstlearn/items/ModItems.java
@@ -6,14 +6,19 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item DAHLIA_SEEDS = registerItem("dahlia_seeds", new Item(new Item.Settings()));
+    public static final Item DAHLIA_SEEDS = registerItem("dahlia_seeds", new Item.Settings());
 
-    private static Item registerItem (String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(FirstLearn.MOD_ID, name), item);
+    private static Item registerItem (String name, Item.Settings itemSettings) {
+        Identifier id = Identifier.of(FirstLearn.MOD_ID, name);
+        RegistryKey<Item> dahliaSeeds = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(dahliaSeeds);
+        return Registry.register(Registries.ITEM, dahliaSeeds, new Item(settings));
     }
 
     public static void registerModItems() {
